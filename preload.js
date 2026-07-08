5eb27363b313576fae72d6253d54225c5033a2c7
Hanni007 <2503008788@qq.com>
2026-07-09 04:17:18 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,4 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
   onSongUpdate: (callback) => ipcRenderer.on('song-update', callback),
+  onConnectionStatus: (callback) => ipcRenderer.on('connection-status', callback),
 });
